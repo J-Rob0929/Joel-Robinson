b5f1e60557648a3d658d1bd9c97cf1347ce592a7
J-Rob0929 <robi0929@algonquinlive.com>
2024-07-30 14:16:14 -0400

    changed to SSH address while trying to publish site

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://J-Rob0929.github.io', // Your website URL
+  url: 'git@github.com:J-Rob0929/Joel-Robinson', // Your website URL
   baseUrl: '/Joel-Robinson', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
